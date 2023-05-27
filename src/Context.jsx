70692702be80ea4ae6b248727476fbbbecac28c3
jealaine0128 <engr.jamielaine@gmail.com>
2023-05-27 16:35:22 +0800

    Testing

--- src/Context.jsx
+++ src/Context.jsx
@@ -17,12 +17,32 @@ const AppProvider = ({ children }) => {
     password: "",
   });
 
+  const [messages, setMessages] = useState([]);
+  const [headerName, setHeaderName] = useState("");
+  const [messageID, setMessageID] = useState("");
+  const [memberList, setMemberList] = useState([]);
+
+  const getData = async (page, id, name) => {
+    setMessagePage('')
+    setHeaderName('')
+    setMessageID(null)
+    setMessagePage(page);
+    setHeaderName(name);
+    setMessageID(id);
+  };
+
   const [authPage, setAuthPage] = useState(false);
+  const [createChannelModal, setCreateChannelModal] = useState(false);
+  const [addChannelMember, setAddChannelMember] = useState(false);
+  const [messagePage, setMessagePage] = useState("");
   const [user, setUser] = useState(
     () => JSON.parse(localStorage.getItem("user")) || null
   );
-
+  const [userChannelList, setUserChannelList] = useState([]);
+  const [memberForm, setMemberForm] = useState("");
+  const [sendMessage, setSendMessage] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const [channelForm, setChannelForm] = useState("");
   const handleRegister = (e) => {
     const { name, value } = e.target;
     setRegForm((prevData) => ({
@@ -31,13 +51,39 @@ const AppProvider = ({ children }) => {
     }));
   };
 
-  
   const handleLogin = (e) => {
     const { name, value } = e.target;
     setLogForm((prevData) => ({
       ...prevData,
       [name]: value,
     }));
+  };
+
+  const addChannelToUser = async (e) => {
+    const { token, client, expiry, uid } = user;
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${API_URL}/channels`,
+        {
+          name: channelForm,
+          user_ids: [],
+        },
+        {
+          headers: {
+            "access-token": token,
+            client: client,
+            expiry: expiry,
+            uid: uid,
+          },
+        }
+      );
+      if (response.data.errors) return alert("Channel name already exist!");
+      setCreateChannelModal(false);
+      getUserChannel();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
 
@@ -60,6 +106,11 @@ const AppProvider = ({ children }) => {
         return alert("Email already exist!");
       }
     }
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    setIsLogin(false);
   };
 
   const loginUser = async (e) => {
@@ -90,17 +141,141 @@ const AppProvider = ({ children }) => {
       if (error.response.data.errors) return alert("Invalid credentials!");
     }
   };
+
+  const getUserChannel = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/channels`, {
+        headers: {
+          "Content-Type": "application/json",
+          "access-token": user.token,
+          client: user.client,
+          expiry: user.expiry,
+          uid: user.uid,
+        },
+      });
+      const { data } = response;
+      setUserChannelList(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    console.log(user);
+    if (user) {
+      setIsLogin(true);
+      localStorage.setItem("user", JSON.stringify(user));
+      const getUser = async () => {
+        await getUserChannel();
+      };
+      getUser();
+    }
+  }, [user]);
 
-  }, [user])
+  const addMessage = async (e) => {
+    e.preventDefault();
+    const { token, client, uid, expiry } = user;
+    try {
+      const response = await axios.post(
+        `${API_URL}/messages`,
+        {
+          receiver_id: messageID,
+          receiver_class: messagePage,
+          body: sendMessage,
+        },
+        {
+          headers: {
+            "access-token": token,
+            expiry: expiry,
+            client: client,
+            uid: uid,
+          },
+        }
+      );
+      setSendMessage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const [channelOwnerID, setChannelOwnerID] = useState('')
 
+  const getAllMember = async () => {
+    const { token, client, expiry, uid } = user;
+    try {
+      const response = await axios.get(`${API_URL}/channels/${messageID}`, {
+        headers: {
+          "access-token": token,
+          client: client,
+          expiry: expiry,
+          uid: uid,
+        },
+      });
+      setChannelOwnerID(response.data.data.owner_id)
+      setMemberList(response.data.data.channel_members);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const addMember = async (e) => {
+    e.preventDefault();
+    const { token, client, expiry, uid } = user;
+    try {
+      const response = await axios.post(
+        `${API_URL}/channel/add_member`,
+        {
+          id: messageID,
+          member_id: memberForm,
+        },
+        {
+          headers: {
+            "access-token": token,
+            client: client,
+            expiry: expiry,
+            uid: uid,
+          },
+        }
+      );
+      getAllMember()
+      setAddChannelMember(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if(user){
+    getUserChannel()
+    }
+  }, [userChannelList])
   return (
     <AppContext.Provider
       value={{
+        memberForm,
+        channelOwnerID,
+        getAllMember,
+        setMemberForm,
+        setMemberList,
+        headerName,
+        getData,
+        sendMessage,
+        setSendMessage,
         user,
+        messageID,
+        memberList,
+        addMessage,
+        messages,
+        logout,
         API_URL,
+        addChannelToUser,
+        messagePage,
+        setChannelForm,
+        setMessagePage,
+        addMember,
+        setMessages,
+        userChannelList,
         handleLogin,
+        setAddChannelMember,
+        addChannelMember,
         isLogin,
         setIsLogin,
         authPage,
@@ -109,6 +284,8 @@ const AppProvider = ({ children }) => {
         handleRegister,
         setRegForm,
         registerUser,
+        createChannelModal,
+        setCreateChannelModal,
         loginUser,
         logForm,
         setLogForm,
